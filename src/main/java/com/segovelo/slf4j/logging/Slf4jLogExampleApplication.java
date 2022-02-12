727c07b96857784e0a8005d8bf1f8108ddf9b1c6
segovelo <gauchosinpingo@gmail.com>
2022-02-12 13:30:29 +0000

    Initial commit

---
+++ src/main/java/com/segovelo/slf4j/logging/Slf4jLogExampleApplication.java
@@ -0,0 +1,13 @@
+package com.segovelo.slf4j.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Slf4jLogExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Slf4jLogExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/segovelo/slf4j/logging/Slf4jLogExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.segovelo.slf4j.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Slf4jLogExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
